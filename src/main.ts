00c1fe96ef91aee813335a0c18fe1351572cebec
Lara Del Coro <laradelcoro01@gmail.com>
2023-11-16 16:49:38 -0300

    cambios en el back

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes( 
     new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     })
    );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
